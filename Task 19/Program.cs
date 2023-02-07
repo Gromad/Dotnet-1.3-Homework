18a7eb449494e199dbbdd849cf4525edd6b265d1
Andrey <mr.kuzyushin@yandex.ru>
2023-02-07 11:42:46 +0500

    working

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -6,3 +6,27 @@
 
 23432 -> да
 */
+
+void CheckPalindorm(string number)
+{
+    int len = number.Length;
+    if (len == 5)
+    {
+        if (number[0] == number[4] && number [1] == number[3])
+        {
+            Console.WriteLine($"{number} - Да. Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - Нет. Это число не палиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено неправильное число");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
