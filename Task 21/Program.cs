9132e18d604d3bb96907685a3a776e9ae2b8f2da
Andrey <mr.kuzyushin@yandex.ru>
2023-02-07 12:30:30 +0500

    Still struggling

---
+++ Task 21/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while (!isCorrect)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+void GetDistance(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double sum1 = Math.Pow((x2 - x1), 2);
+    double sum2 = Math.Pow((y2 - y1), 2);
+    double sum3 = Math.Pow((z2 - z1), 2);
+
+    double result = Math.Sqrt(sum1 + sum2 + sum3);
+
+    Console.WriteLine($"Расстояние между двумя точками равно {result.ToString("#.00)}.");
+
+}
+
+int x1 = GetNumber("Введите координату первой точки Х1: ");
+int y1 = GetNumber("Введите координату первой точки Y1: ");
+int z1 = GetNumber("Введите координату первой точки Z1: ");
+int x1 = GetNumber("Введите координату второй точки Х1: ");
+int y1 = GetNumber("Введите координату второй точки Y1: ");
+int z1 = GetNumber("Введите координату второй точки Z1: ");
+
+GetDistance(x1, x2, y1, y2, z1, z2);
